156d2666ed5b1438705f6bfc1b88e52569b877b4
andy lien <andy922200@gmail.com>
2021-12-08 18:30:37 +0800

    1. init project

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VueSidebar from '@/vue-sidebar.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    VueSidebar
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-sidebar />
+  </div>
+</template>
